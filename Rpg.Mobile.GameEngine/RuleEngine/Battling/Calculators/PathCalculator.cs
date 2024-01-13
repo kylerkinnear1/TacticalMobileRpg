21747cd6cc8b9b81548a9565bd1ed3c54756d0be
Kyle Kinnear <kylerkinnear@gmail.com>
2024-01-13 11:04:55 -0600

    Started path logic. Could improve performance but g2g.

--- Rpg.Mobile.GameEngine/RuleEngine/Battling/Calculators/PathCalculator.cs
+++ Rpg.Mobile.GameEngine/RuleEngine/Battling/Calculators/PathCalculator.cs
@@ -4,12 +4,35 @@ namespace Rpg.Mobile.GameEngine.RuleEngine.Battling.Calculators;
 
 public interface IPathCalculator
 {
-    int ShortestDistance(Point a, Point b);
+    int Distance(Point a, Point b);
+    IEnumerable<Point> FanOutArea(Point source, Point bounds, int range);
 }
 
 public class PathCalculator : IPathCalculator
 {
-    public int ShortestDistance(Point a, Point b) => ShortestDistance(a, b, Enumerable.Empty<Point>());
+    public int Distance(Point a, Point b)
+    {
+        var xDistance = Math.Abs(a.X - b.X);
+        var yDistance = Math.Abs(a.Y - b.Y);
+        return xDistance + yDistance;
+    }
 
-    public int ShortestDistance(Point a, Point b, IEnumerable<Point> blockedPoints) => throw new NotImplementedException();
+    public IEnumerable<Point> FanOutArea(Point source, Point bounds, int range)
+    {
+        var xRanges = Enumerable.Range(
+            Math.Max(0, source.X - range),
+            Math.Min(bounds.X, source.X - range));
+
+        var yRanges = Enumerable.Range(
+            Math.Max(0, source.Y - range),
+            Math.Min(bounds.Y, source.Y + range));
+
+        var legalPoints = xRanges
+            // TODO: Better yRanges here for more performance.
+            .SelectMany(x => yRanges.Select(y => new Point(x, y)))
+            .Where(pos => Distance(source, pos) <= range)
+            .ToList();
+
+        return legalPoints;
+    }
 }
--- Rpg.Mobile.GameEngine/RuleEngine/Battling/Calculators/RangeCalculator.cs
+++ Rpg.Mobile.GameEngine/RuleEngine/Battling/Calculators/RangeCalculator.cs
@@ -17,31 +17,13 @@ public class RangeCalculator : IRangeCalculator
 
     public IEnumerable<Point> GetLegalTargets(Board board, Unit activeUnit, TargetSource source)
     {
-        var xRanges = Enumerable.Range(
-            Math.Max(0, activeUnit.Position.X - source.Range),
-            Math.Min(board.Bounds.X, activeUnit.Position.X + source.Range));
-
-        var yRanges = Enumerable.Range(
-            Math.Max(0, activeUnit.Position.Y - source.Range),
-            Math.Min(board.Bounds.Y, activeUnit.Position.Y + source.Range));
-
-        var enemyLocations = board.Units
-            .Where(x => activeUnit.PlayerId != x.Value.PlayerId)
-            .ToDictionary(x => x.Value.Position);
-
-        var friendlyLocations = board.Units
-            .Where(x => activeUnit.PlayerId == x.Value.PlayerId)
-            .ToDictionary(x => x.Value.Position);
-
-        var pointsInRange = xRanges
-            .SelectMany(x => yRanges
-                .Select(y => new Point(x, y)))
+        var unitLocations = board.Units.ToDictionary(x => x.Value.Position);
+        var targetArea = _path.FanOutArea(activeUnit.Position, board.Bounds, source.Range);
+        return targetArea
             .Where(x =>
-                (source.TargetsEnemies || !enemyLocations.ContainsKey(x)) &&
-                (source.TargetsFriendlies || !friendlyLocations.ContainsKey(x)) &&
-                _path.ShortestDistance(activeUnit.Position, x) <= source.Range)
+                unitLocations.TryGetValue(x, out var unit) &&
+                ((source.TargetsEnemies && unit.Value.PlayerId != source.ActiveUnit.PlayerId) ||
+                 (source.TargetsFriendlies && unit.Value.PlayerId == source.ActiveUnit.PlayerId)))
             .ToList();
-
-        return pointsInRange;
     }
 }
