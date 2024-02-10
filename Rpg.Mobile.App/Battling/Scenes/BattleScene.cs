9ffa700871e9fcf8d6fb97d4f92f527cdcb3e56c
Kyle Kinnear <kylerkinnear@gmail.com>
2024-02-10 06:35:58 -0600

    Made a list to do updates and renders. Controlled by order added.

--- Rpg.Mobile.App/Battling/Scenes/BattleScene.cs
+++ Rpg.Mobile.App/Battling/Scenes/BattleScene.cs
@@ -6,36 +6,42 @@ namespace Rpg.Mobile.App.Battling.Scenes;
 public class BattleScene : IScene, IDrawable
 {
     private readonly GenericUnitState _unitState;
-    private readonly GenericUnitGameObject _unitGameObject;
-
     private readonly GridState _gridState;
-    private readonly GridGameObject _gridGameObject;
     private readonly IGraphicsView _view;
+
+    private readonly List<IUpdateGameObject> _updates = new();
+    private readonly List<IRenderGameObject> _renders = new();
 
     public BattleScene(IGraphicsView view)
     {
         var spriteLoader = new EmbeddedResourceImageLoader(new(GetType().Assembly));
         var warriorSprite = spriteLoader.Load("Warrior.png");
         _unitState = new GenericUnitState(new(100f, 100f), warriorSprite, 5f);
-        _unitGameObject = new GenericUnitGameObject(_unitState);
-
         _gridState = new GridState(new(0f, 0f), 60, 30, 20);
-        _gridGameObject = new GridGameObject(_gridState);
+
+        var genericGameObject = new GenericUnitGameObject(_unitState);
+        var gridGameObject = new GridGameObject(_gridState);
+
+        _updates.Add(genericGameObject);
+        _updates.Add(gridGameObject);
+
+        _renders.Add(genericGameObject);
+        _renders.Add(gridGameObject);
 
         _view = view;
     }
 
     public void Update(TimeSpan delta)
     {
-        _gridGameObject.Update(delta);
-        _unitGameObject.Update(delta);
+        foreach (var update in _updates) 
+            update.Update(delta);
     }
 
     public void Render() => _view.Invalidate();
 
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
-        _gridGameObject.Render(canvas, dirtyRect);
-        _unitGameObject.Render(canvas, dirtyRect);
+        foreach (var render in _renders)
+            render.Render(canvas, dirtyRect);
     }
 }
