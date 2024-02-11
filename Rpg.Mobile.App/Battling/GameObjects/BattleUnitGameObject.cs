8b96edfcc8d113b670947cd3649193ddcac44e0a
Kyle Kinnear <kylerkinnear@gmail.com>
2024-02-10 19:08:52 -0600

    Adding new grid based unit. Taking a break. Going to make it move to wherever is clicked.

---
+++ Rpg.Mobile.App/Battling/GameObjects/BattleUnitGameObject.cs
@@ -0,0 +1,48 @@
+﻿using Rpg.Mobile.App.Battling.Scenes;
+using Rpg.Mobile.GameSdk;
+using Rpg.Mobile.GameSdk.Extensions;
+using IImage = Microsoft.Maui.Graphics.IImage;
+
+namespace Rpg.Mobile.App.Battling.GameObjects;
+
+public class BattleUnitState
+{
+    public bool IsVisible { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public IImage Sprite { get; set; }
+
+    public BattleUnitState(
+        IImage sprite, 
+        int x = 0,
+        int y = 0,
+        bool isVisible = true)
+    {
+        IsVisible = isVisible;
+        X = x;
+        Y = y;
+        Sprite = sprite;
+    }
+}
+
+public class BattleUnitGameObject : IGameObject
+{
+    private readonly BattleSceneState _scene;
+    private readonly BattleUnitState _state;
+
+    public BattleUnitGameObject(BattleSceneState scene, BattleUnitState state)
+    {
+        _scene = scene;
+        _state = state;
+    }
+
+
+    public void Update(TimeSpan delta)
+    {
+    }
+
+    public void Render(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.Draw(_state.Sprite, new(_state.X * _scene.Grid.Size, _state.Y * _scene.Grid.Size), .5f);
+    }
+}
