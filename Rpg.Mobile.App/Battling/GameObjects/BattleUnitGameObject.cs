3077dea7d745b436ad59613030ca9900327c4676
Kyle Kinnear <kylerkinnear@gmail.com>
2024-02-16 12:43:53 -0600

    Current state.

--- Rpg.Mobile.App/Battling/GameObjects/BattleUnitGameObject.cs
+++ Rpg.Mobile.App/Battling/GameObjects/BattleUnitGameObject.cs
@@ -49,10 +49,11 @@ public class BattleUnitGameObject : IGameObject
     {
         if (!_state.IsVisible)
             return;
-
+        
         var x = _state.Position.X * _scene.Grid.Size + _scene.Grid.Position.X;
         var y = _state.Position.Y * _scene.Grid.Size + _scene.Grid.Position.Y;
         canvas.Draw(_state.Sprite, new(x, y - 3f), _state.Scale);
+
         canvas.Font = _state.StatusFont;
         canvas.FontSize = 22f;
         canvas.FontColor = _state.PlayerId == 0 ? Colors.Aqua : Colors.Orange;
