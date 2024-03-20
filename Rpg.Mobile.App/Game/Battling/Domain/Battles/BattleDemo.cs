0d45ec19007907ae61685f56950067ccb4574928
Kyle Kinnear <kylerkinnear@gmail.com>
2024-03-20 17:14:15 -0500

    Added map.json, so the map can be updated without making a new build.

--- Rpg.Mobile.App/Game/Battling/Domain/Battles/BattleDemo.cs
+++ Rpg.Mobile.App/Game/Battling/Domain/Battles/BattleDemo.cs
@@ -1,7 +1,15 @@
 ﻿using Rpg.Mobile.GameSdk;
-using Point = System.Drawing.Point;
+using System.Text.Json;
 
 namespace Rpg.Mobile.App.Game.Battling.Domain.Battles;
+
+public record Point32(int X, int Y);
+public record MapJson(
+    int RowCount,
+    int ColumnCount,
+    List<Point32> Player1Origins,
+    List<Point32> Player2Origins,
+    List<Point32> RockPositions);
 
 public static partial class Battles
 {
@@ -9,37 +17,49 @@ public static partial class Battles
 
     private static MapState CreateDemo()
     {
-        var state = MapState.New(10, 12);
+        var mapJson = LoadMap();
+
+        var state = MapState.New(mapJson.RowCount, mapJson.ColumnCount);
         var player1Units = StatPresets.All.Shuffle(Rng.Instance).ToList();
         var player2Units = StatPresets.All.Shuffle(Rng.Instance).ToList();
         player2Units.ForEach(x => x.PlayerId = 1);
 
-        var player1StartingPositions = new List<Point>
-        {
-            new(1, 1), new(1, 3), new(1, 5), new(1, 7), new(1, 9)
-        };
+        foreach (var (unit, point) in player1Units.Zip(mapJson.Player1Origins))
+            state.UnitTiles[unit] = new(point.X, point.Y);
 
-        var player2StartingPositions = new List<Point>
-        {
-            new(8, 1), new(8, 3), new(8, 5), new(8, 7), new(8, 9)
-        };
+        foreach (var (unit, point) in player2Units.Zip(mapJson.Player2Origins))
+            state.UnitTiles[unit] = new(point.X, point.Y);
 
-        foreach (var (unit, point) in player1Units.Zip(player1StartingPositions))
-            state.UnitTiles[unit] = point;
-
-        foreach (var (unit, point) in player2Units.Zip(player2StartingPositions))
-            state.UnitTiles[unit] = point;
-
-        var rockPositions = new List<Point>
-        {
-            new(3, 5), new(6, 4), new(6, 5), new(3, 4), new(2, 2), new(7, 9), new(4, 8), new(5, 8)
-        };
-
-        rockPositions.ForEach(x => state.Tiles[x.X, x.Y].Type = TerrainType.Rock);
+        mapJson.RockPositions.ForEach(x => state.Tiles[x.X, x.Y].Type = TerrainType.Rock);
 
         var allUnits = player1Units.Concat(player2Units).Shuffle(Rng.Instance).ToList();
         state.TurnOrder.AddRange(allUnits);
 
         return state;
     }
+
+    private static MapJson? LoadMap()
+    {
+        var mapPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "map.json");
+        if (!File.Exists(mapPath)) 
+            return BuildDefaultMap();
+
+        var mapText = File.ReadAllText(mapPath);
+        return JsonSerializer.Deserialize<MapJson>(mapText);
+    }
+
+    private static MapJson BuildDefaultMap() =>
+        new(10, 12,
+            new List<Point32>
+            {
+                new(1, 1), new(1, 3), new(1, 5), new(1, 7), new(1, 9)
+            },
+            new List<Point32>
+            {
+                new(8, 1), new(8, 3), new(8, 5), new(8, 7), new(8, 9)
+            },
+            new List<Point32>
+            {
+                new(3, 5), new(6, 4), new(6, 5), new(3, 4), new(2, 2), new(7, 9), new(4, 8), new(5, 8)
+            });
 }
