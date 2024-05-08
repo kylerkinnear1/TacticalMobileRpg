013b486deb072c9bf4e9f049b485449af4c539b6
Kyle Kinnear <kylerkinnear@gmail.com>
2024-05-07 22:42:40 -0500

    Tweak visuals.

--- Rpg.Mobile.App/Game/Battling/Components/BattleUnitComponent.cs
+++ Rpg.Mobile.App/Game/Battling/Components/BattleUnitComponent.cs
@@ -15,7 +15,6 @@ public class BattleUnitComponent : SpriteComponentBase
         State = state;
 
         HealthBar = AddChild(new BattleUnitHealthBarComponent(State));
-        HealthBar.FontColor = State.PlayerId == 0 ? Colors.Aqua : Colors.Orange;
         HealthBar.Position = new(-10f, Sprite.Height - HealthBar.Bounds.Height + 10f);
     }
 }
@@ -24,7 +23,6 @@ public class BattleUnitHealthBarComponent : ComponentBase
 {
     public BattleUnitState State { get; }
     public Font Font { get; set; } = new("Arial", FontWeights.ExtraBold, FontStyleType.Italic);
-    public Color FontColor { get; set; } = Colors.Orange;
     public bool HasGone { get; set; } = false;
 
     public BattleUnitHealthBarComponent(BattleUnitState state) : base(new(0f, 0f, 30f, 25f))
@@ -34,19 +32,29 @@ public class BattleUnitHealthBarComponent : ComponentBase
 
     public override void Update(float deltaTime)
     {
-
+        Visible = State.RemainingHealth > 0;
     }
 
     public override void Render(ICanvas canvas, RectF dirtyRect)
     {
-        if (State.RemainingHealth <= 0)
-            return;
-
         canvas.Font = Font;
         canvas.FontSize = HasGone ? 19f : 22f;
-        canvas.FontColor = FontColor.WithAlpha(HasGone ? 0.8f : 1.0f);
-        canvas.FillColor = HasGone ? Colors.Black.WithAlpha(0.5f) : Colors.SlateGray.WithAlpha(.7f);
-        canvas.FillRoundedRectangle(0f, 0f, Bounds.Width, Bounds.Height, 2f);
-        canvas.DrawString($"{State.RemainingHealth}", 0f, 20f, HorizontalAlignment.Left);
+
+        canvas.FillColor = HasGone ? Colors.SlateGray.WithAlpha(.8f) : Colors.Black.WithAlpha(.6f);
+        canvas.FontColor = State.PlayerId switch
+        {
+            0 when !HasGone => Colors.Aqua,
+            1 when !HasGone => Colors.Orange,
+            0 when HasGone => Colors.DarkBlue,
+            1 when HasGone => Colors.Brown,
+            _ => throw new ArgumentException()
+        };
+
+        canvas.StrokeColor = State.PlayerId == 0 ? Colors.Aqua : Colors.Orange;
+        canvas.StrokeSize = 1f;
+
+        var bounds = new RectF(PointF.Zero, Bounds.Size);
+        canvas.FillRoundedRectangle(bounds, 2f);
+        canvas.DrawString($"{State.RemainingHealth}", bounds, HorizontalAlignment.Center, VerticalAlignment.Center);
     }
 }
