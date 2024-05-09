a9b60c32954a8740a5c80abf6a7a6f93166c4ec8
Kyle Kinnear <kylerkinnear@gmail.com>
2024-05-08 23:25:03 -0500

    Created some handlers to make service smaller. Need to clean up dependencies a bit, but this is more manageable and coupling is easier to see.

--- Rpg.Mobile.App/Game/Battling/Gamemaster/BattleState.cs
+++ Rpg.Mobile.App/Game/Battling/Gamemaster/BattleState.cs
@@ -47,6 +47,9 @@ public class BattleStateService
     private readonly StartBattleHandler _startBattle;
     private readonly AdvanceToNextUnitHandler _advanceUnit;
     private readonly ChangeBattleStateHandler _changeStep;
+    private readonly SelectTileHandler _selectTile;
+    private readonly TargetSpellHandler _targetSpell;
+    private readonly ValidTargetCalculator _validTargetCalculator;
 
     private BattleUnitState CurrentUnit => _state.TurnOrder[_state.ActiveUnitIndex];
 
@@ -58,6 +61,10 @@ public class BattleStateService
         _changeStep = new(_state, _path);
         _advanceUnit = new(_state, _changeStep);
         _startBattle = new(_state, _advanceUnit);
+        _selectTile = new(_state, _path, _advanceUnit);
+        _targetSpell = new(_state, _changeStep);
+
+        _validTargetCalculator = new ValidTargetCalculator(_state);
     }
 
     public void StartBattle() => _startBattle.Handle();
@@ -66,123 +73,12 @@ public class BattleStateService
 
     public void ChangeBattleState(BattleStep step) => _changeStep.Handle(step);
 
-    public void SelectTile(Point tile)
-    {
-        if (_state.Step == BattleStep.Moving && _state.WalkableTiles.Contains(tile))
-        {
-            _state.UnitCoordinates[CurrentUnit] = tile;
-            Bus.Global.Publish(new UnitMovedEvent(CurrentUnit, tile));
-        }
+    public void SelectTile(Point tile) => _selectTile.Handle(tile);
 
-        if (_state.Step == BattleStep.SelectingAttackTarget)
-        {
-            var enemy = _state.UnitsAt(tile).FirstOrDefault(x => x.PlayerId != _state.CurrentUnit.PlayerId);
-            if (enemy is null)
-                return;
+    public void TargetSpell(SpellState spell) => _targetSpell.Handle(spell);
 
-            var damage = CalcAttackDamage(_state.CurrentUnit.Stats.Attack, enemy.Stats.Defense);
-            DamageUnits(new [] { enemy }, damage);
-        }
-
-        if (_state.Step == BattleStep.SelectingMagicTarget && _state.CurrentSpell is not null)
-        {
-            CastSpell(_state.CurrentSpell, tile);
-        }
-    }
-
-    public int CalcAttackDamage(int attack, int defense)
-    {
-        var deterministicDamage = Math.Max(1, attack - defense);
-        var damageRangeModifier = Rng.Instance.Double(0.25) * deterministicDamage;
-
-        var damage = deterministicDamage + (int)Math.Round(damageRangeModifier);
-        return Math.Max(1, damage);
-    }
-
-    public int CalcSpellDamage(SpellState spell) =>
-        spell.Type switch
-        {
-            SpellType.Fire1 => Rng.Instance.Int(6, 8),
-            SpellType.Fire2 => Rng.Instance.Int(7, 9),
-            SpellType.Cure1 => -6,
-            _ => throw new ArgumentException()
-        };
-
-    public void CastSpell(SpellState spell, Point target)
-    {
-        var hits = _path
-            .CreateFanOutArea(target, _state.Map.Corner, spell.MinRange - 1, spell.MaxRange - 1)
-            .ToHashSet();
-
-        var units = _state.UnitCoordinates.Where(x => hits.Contains(x.Value));
-        var targets = units
-            .Where(x =>
-                x.Key.PlayerId == CurrentUnit.PlayerId && spell.TargetsFriendlies ||
-                x.Key.PlayerId != CurrentUnit.PlayerId && spell.TargetsEnemies)
-            .ToList();
-
-        CastSpell(spell, targets.Select(x => x.Key));
-    }
-
-    public void CastSpell(SpellState spell, IEnumerable<BattleUnitState> targets)
-    {
-        if (CurrentUnit.RemainingMp < spell.MpCost)
-        {
-            Bus.Global.Publish(new NotEnoughMpEvent(spell));
-            return;
-        }
-
-        var damage = CalcSpellDamage(spell);
-        CurrentUnit.RemainingMp -= spell.MpCost;
-        DamageUnits(targets, damage);
-    }
-
-    public void TargetSpell(SpellState spell)
-    {
-        _state.CurrentSpell = spell;
-        ChangeBattleState(BattleStep.SelectingMagicTarget);
-    }
-
-    public void DamageUnits(IEnumerable<BattleUnitState> units, int damage)
-    {
-        var defeatedUnits = new List<BattleUnitState>();
-        var damagedUnits = new List<(BattleUnitState, int)>();
-        foreach (var unit in units)
-        {
-            unit.RemainingHealth = damage >= 0
-                ? Math.Max(unit.RemainingHealth - damage, 0)
-                : Math.Min(unit.Stats.MaxHealth, unit.RemainingHealth - damage);
-
-            damagedUnits.Add((unit, damage));
-
-            if (unit.RemainingHealth <= 0)
-            {
-                defeatedUnits.Add(unit);
-            }
-        }
-
-        Bus.Global.Publish(new UnitDamagedEvent(damagedUnits));
-        Bus.Global.Publish(new UnitsDefeatedEvent(defeatedUnits));
-
-        AdvanceToNextUnit();
-    }
-
-    public bool IsValidMagicTargetTile(Point tile)
-    {
-        var hoveredUnit = _state.UnitsAt(tile).FirstOrDefault();
-        return _state.Step == BattleStep.SelectingMagicTarget &&
-               hoveredUnit != null &&
-               ((_state.CurrentSpell.TargetsEnemies && hoveredUnit.PlayerId != CurrentUnit.PlayerId) ||
-                (_state.CurrentSpell.TargetsFriendlies && hoveredUnit.PlayerId == CurrentUnit.PlayerId));
-    }
-
-    public bool IsValidAttackTargetTile(Point tile)
-    {
-        var hoveredUnit = _state.UnitsAt(tile).FirstOrDefault();
-        return _state.Step == BattleStep.SelectingAttackTarget &&
-               hoveredUnit != null &&
-               hoveredUnit.PlayerId != CurrentUnit.PlayerId;
-    }
+    public bool IsValidMagicTargetTile(Point tile) => _validTargetCalculator.IsValidMagicTargetTile(tile);
+    public bool IsValidAttackTargetTile(Point tile) => _validTargetCalculator.IsValidAttackTargetTile(tile);
 
     public void RerollUnit()
     {
---
+++ Rpg.Mobile.App/Game/Battling/Gamemaster/Handlers/ValidTargetCalculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Rpg.Mobile.App.Game.Battling.Gamemaster.Handlers;
+
+public class ValidTargetCalculator
+{
+    private readonly BattleState _state;
+
+    public ValidTargetCalculator(BattleState state) => _state = state;
+
+    public bool IsValidMagicTargetTile(Point tile)
+    {
+        var hoveredUnit = _state.UnitsAt(tile).FirstOrDefault();
+        return _state.Step == BattleStep.SelectingMagicTarget &&
+               hoveredUnit != null &&
+               ((_state.CurrentSpell.TargetsEnemies && hoveredUnit.PlayerId != _state.CurrentUnit.PlayerId) ||
+                (_state.CurrentSpell.TargetsFriendlies && hoveredUnit.PlayerId == _state.CurrentUnit.PlayerId));
+    }
+
+    public bool IsValidAttackTargetTile(Point tile)
+    {
+        var hoveredUnit = _state.UnitsAt(tile).FirstOrDefault();
+        return _state.Step == BattleStep.SelectingAttackTarget &&
+               hoveredUnit != null &&
+               hoveredUnit.PlayerId != _state.CurrentUnit.PlayerId;
+    }
+}
