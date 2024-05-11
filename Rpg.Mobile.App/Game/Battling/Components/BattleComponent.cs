516a962e867994ca9690274eb79ff2fb912ad01f
Kyle Kinnear <kylerkinnear@gmail.com>
2024-05-11 08:45:33 -0500

    Remove test message.

--- Rpg.Mobile.App/Game/Battling/Components/BattleComponent.cs
+++ Rpg.Mobile.App/Game/Battling/Components/BattleComponent.cs
@@ -152,8 +152,6 @@ public class BattleComponent : ComponentBase
 
         var point = _state.UnitCoordinates[component.State];
         component.Position = GetPositionForTile(point, component.Bounds.Size);
-
-        ShowMessage("test");
     }
 
     private void TileClicked(TileClickedEvent evnt)
