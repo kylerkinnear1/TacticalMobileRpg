942692b4d2929fc18b78c2d306715ea01e167443
Kyle Kinnear <kylerkinnear@gmail.com>
2024-09-02 17:22:53 -0500

    Fix some setup issue.

--- Rpg.Mobile.App/Game/MainBattle/StateMachines/Phases/NewRound/NewRoundPhase.cs
+++ Rpg.Mobile.App/Game/MainBattle/StateMachines/Phases/NewRound/NewRoundPhase.cs
@@ -8,7 +8,11 @@ public class NewRoundPhase(Context _context) : IBattlePhase
 {
     public void Enter()
     {
-        _context.Data.TurnOrder.Set(_context.Data.TurnOrder.Shuffle(Rng.Instance).ToList());
+        var availableUnits = _context.Data.TurnOrder.Count == 0
+            ? _context.Data.PlaceOrder
+            : _context.Data.TurnOrder;
+        
+        _context.Data.TurnOrder.Set(availableUnits.Shuffle(Rng.Instance).ToList());
         _context.Main.Units.Values.ToList().ForEach(x => x.Unit.HealthBar.HasGone = false);
         _context.Data.ActiveUnitIndex = 0;
         _context.Data.ActiveUnitStartPosition = _context.Data.UnitCoordinates[_context.Data.CurrentUnit];
--- Rpg.Mobile.App/Game/MainBattle/StateMachines/Phases/Setup/SetupPhase.cs
+++ Rpg.Mobile.App/Game/MainBattle/StateMachines/Phases/Setup/SetupPhase.cs
@@ -26,7 +26,10 @@ public class SetupPhase(Context _context) : IBattlePhase
         [
             Bus.Global.Subscribe<TileHoveredEvent>(TileHovered),
             Bus.Global.Subscribe<TileClickedEvent>(TileClicked)
-        ];       
+        ];
+
+        var firstUnit = _context.Data.PlaceOrder[0];
+        _context.Main.PlaceUnitSpriteComponent.Sprite = GetUnitSprite(firstUnit.Stats.UnitType, firstUnit.PlayerId);
     }
 
     public void Execute(float deltaTime)
@@ -86,24 +89,33 @@ public class SetupPhase(Context _context) : IBattlePhase
         component.Position = _context.Main.GetPositionForTile(point, component.Bounds.Size);
 
         Bus.Global.Publish(new UnitPlacedEvent(unit));
+
+        if (_context.Data.CurrentPlaceOrder < _context.Data.PlaceOrder.Count)
+        {
+            var nextUnit = _context.Data.PlaceOrder[_context.Data.CurrentPlaceOrder];
+            _context.Main.PlaceUnitSpriteComponent.Sprite = GetUnitSprite(nextUnit.Stats.UnitType, nextUnit.PlayerId);
+        }
     }
 
     private bool IsPlaceableTile(Point? hover, ICollection<Point> currentOrigins) => 
         hover.HasValue && currentOrigins.Contains(hover.Value);
 
     private static BattleUnitComponent CreateBattleUnitComponent(BattleUnitData state) =>
-        (state.Stats.UnitType, state.PlayerId) switch
+        new(GetUnitSprite(state.Stats.UnitType, state.PlayerId));
+
+    private static IImage GetUnitSprite(BattleUnitType type, int playerId) =>
+        (type, playerId) switch
         {
-            (BattleUnitType.Archer, 0) => new BattleUnitComponent(Images.ArcherIdle01),
-            (BattleUnitType.Healer, 0) => new BattleUnitComponent(Images.HealerIdle01),
-            (BattleUnitType.Mage, 0) => new BattleUnitComponent(Images.MageIdle01),
-            (BattleUnitType.Warrior, 0) => new BattleUnitComponent(Images.WarriorIdle01),
-            (BattleUnitType.Ninja, 0) => new BattleUnitComponent(Images.NinjaIdle01),
-            (BattleUnitType.Archer, 1) => new BattleUnitComponent(Images.ArcherIdle02),
-            (BattleUnitType.Healer, 1) => new BattleUnitComponent(Images.HealerIdle02),
-            (BattleUnitType.Mage, 1) => new BattleUnitComponent(Images.MageIdle02),
-            (BattleUnitType.Warrior, 1) => new BattleUnitComponent(Images.WarriorIdle02),
-            (BattleUnitType.Ninja, 1) => new BattleUnitComponent(Images.NinjaIdle02),
+            (BattleUnitType.Archer, 0) => Images.ArcherIdle01,
+            (BattleUnitType.Healer, 0) => Images.HealerIdle01,
+            (BattleUnitType.Mage, 0) =>  Images.MageIdle01,
+            (BattleUnitType.Warrior, 0) =>  Images.WarriorIdle01,
+            (BattleUnitType.Ninja, 0) =>  Images.NinjaIdle01,
+            (BattleUnitType.Archer, 1) =>  Images.ArcherIdle02,
+            (BattleUnitType.Healer, 1) =>  Images.HealerIdle02,
+            (BattleUnitType.Mage, 1) =>  Images.MageIdle02,
+            (BattleUnitType.Warrior, 1) =>  Images.WarriorIdle02,
+            (BattleUnitType.Ninja, 1) =>  Images.NinjaIdle02,
             _ => throw new ArgumentException()
         };
 }
